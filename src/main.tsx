96524adb9302e2e69c371fcd8b4230e73e7c0c72
zuhairzeb <Zebzuhair71@gmail.com>
2026-06-15 10:19:12 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "public/index.css";
+// @ts-ignore: CSS import declarations not available in this project setup
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
